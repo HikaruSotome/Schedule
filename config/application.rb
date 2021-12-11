5a104653128adcecb47c7cd76a8187a9eb0f559e
Ubuntu <ubuntu@ip-172-31-24-202.ap-northeast-1.compute.internal>
2021-12-11 17:33:21 +0000

    スケジュールアプリ

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Schedule
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    I18n.enforce_available_locales = false
+    config.i18n.default_locale = :ja
   end
 end
